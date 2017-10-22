a3508a0ef425d6affbcf5d50ddbaa59566eb92c9
anicalymus <anicalymus@gmail.com>
2017-10-22 14:08:21 -0700

    Made some changes on seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,14 @@
 require 'faker'
 
-users = 5.times do 
-		User.create(first_name: Faker::Name.first_name
-					last_name: 	Faker::Name.last_name
-					email: 		Faker::Internet.email
-					password: 'password')
-	end
+user = 5.times do 
+	User.create(first_name: Faker::Name.first_name
+				last_name: 	Faker::Name.last_name
+				email: 		Faker::Internet.email
+				password: 'password')
+end
 
 
-ingredients = 5.times do
-	Ingredient.create(name: Faker::Food.ingredient)
+recipe = 5.times do
+	Recipe.create(name: Faker::Food.dish)
+end
 
-
-		Recipe.create(name: Faker::Food.recipe)
-		Saved_Recipe.create(name: Faker::Food.saved_recipe)
