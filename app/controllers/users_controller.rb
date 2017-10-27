19e9f7952e46c2fd6b7f8366fa78a3aee50d64f3
anicalymus <anicalymus@gmail.com>
2017-10-26 23:10:08 -0700

    Ajaxify user login and sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,33 @@
 get "/users/new" do
-	erb :"users/new"
+	if request.xhr? 
+		erb :'_sign_up', layout: false
+	else
+		erb :'users/new'
+	end
 end
 
 post "/users" do
 	@user = User.new(params[:user])
 	if @user.save
-		session[:id] = @user.id
-		redirect "/users/#{@user.id}"
+		if request.xhr?
+			erb :'_sign_up', layout: false, locals: {user: @user}
+		# session[:id] = @user.id
+		else
+			redirect "/users/#{@user.id}"
+		end
 	else
 		@errors = @user.errors.full_messages
-		erb :"users/new"
+		erb :"_errors", layout: false
 	end
 end
 
 get "/users/:id" do
 	@user = User.find(params[:id])
-	if session[:id] == @user.id
-		erb :"users/show"
+	if request.xhr?
+		erb :"_input_ingredient", layout: false
+	# if session[:id] == @user.id
 	else
-		erb :"sessions/new"
+		erb :"users/show"
+	# erb :"sessions/new"
 	end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,95 @@
 $(document).ready(function() {
-  
+	loginSubmit();
+	loginShow();
+	// signUpSubmit();
+	signUpShow();
+	inputIngredient();
 });
+
+var loginSubmit = function(){
+	$(".login-form").on("submit", function(e){
+		e.preventDefault();
+
+		var loginSubmit = $(this);
+		var url = loginSubmit.attr("action");
+		var method = loginSubmit.attr("method");
+		var data = loginSubmit.serialize();
+		$loginSubmit.find('.login-submit').attr('value')
+
+		var request = $.ajax({
+			url: url,
+			method: method,
+			data: data
+		})
+		request.done(function(response){
+			form.before(response);
+		})
+		request.fail(function(error){
+		})
+		$loginSubmit.parent().find('.login-submit').text(response);
+	})
+};
+
+
+var loginShow = function() { 
+	$(".login-show-link").on("click", function(e) {
+		e.preventDefault();
+
+		var loginShow = $(this)
+		var url = loginShow.attr("href")
+		
+		var request = $.ajax({
+			url: url, 
+			method: 'get'
+		})
+		request.done(function(response){
+			$('.login-links').replaceWith(response);
+		})
+
+	})
+}
+
+// var signUpSubmit = function(){
+
+// }
+var signUpShow = function(){
+	$(".sign-up-form").on("click", function(e){
+		e.preventDefault();
+
+		var signUpShow = $(this);
+		var url = signUpShow.attr("href");
+
+		var request = $.ajax({
+			url: url,
+			method: 'get'
+		})
+		request.done(function(response){
+			$('.sign-up-form').replaceWith(response);
+		})
+		request.fail(function(error){
+		})
+
+	})
+};
+
+var inputIngredient = function(){
+	$(".input-ingredient").on("submit", function(e){
+		e.preventDefault();
+
+		var ingredientForm = $(this);
+		var url = ingredientForm.attr("action");
+		var method = ingredientForm.attr("method");
+		var data = ingredientForm.serialize();
+
+		var request = $.ajax({
+			url: url,
+			method: method,
+			data: data
+		})
+		request.done(function(response){
+			form.before(response);
+		})
+		request.fail(function(error){
+		})
+	})
+};
